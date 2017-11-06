bcbfb3906e3f6ee241be892c69ac358738eb0491
Elliot Kahan <ejkahan@gmail.com>
2017-11-06 10:51:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,8 @@ const app = "I don't do much."
 
 var kittens = ['Milo', 'Otis', 'Garfield']
 
-function kittens(kittens){
-  return kittens
-}
-
 function destructivelyAppendKitten(kittens){
 
 return kittens.push("Ralph")
     }
+    
