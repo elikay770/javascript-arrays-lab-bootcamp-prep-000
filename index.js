e310fff314812e07bbd131b0d58b5d42d823fe08
Elliot Kahan <ejkahan@gmail.com>
2017-11-06 10:39:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much."
 
-function destructivelyAppendKitten(kittens){
-kittens.push("Ralph")
-    return kittens
+function destructivelyAppendKitten(array){
+array.push("Ralph")
+    return array
     }
